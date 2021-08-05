f614579ecdf1c1299b35c86ce1048faf93a403fe
yuus95 <kkad45@naver.com>
2021-08-05 15:39:26 +0900

    ec2를 활용한 기능구현 프로젝트

---
+++ src/test/java/com/yushin/YushinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yushin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YushinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
