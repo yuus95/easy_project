5357943fe58e202cd879c04197b45f30a4d3fd48
yuus95 <kkad45@naver.com>
2021-08-13 18:52:46 +0900

    0813 비밀번호 오류 예외처리 완료 시큐리티 공부하기

--- src/main/java/com/yushin/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/yushin/jwt/JwtAuthenticationEntryPoint.java
@@ -1,11 +1,15 @@
 package com.yushin.jwt;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yushin.handler.ex.CustomException;
 import com.yushin.handler.ex.ErrorCode;
 import com.yushin.handler.ex.ErrorResponse;
+import com.yushin.web.dto.LoginDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,8 +17,10 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
 
-import static com.yushin.handler.ex.ErrorCode.DUPLICATE_MEMBER;
+import static com.yushin.handler.ex.ErrorCode.*;
 
 /**
  * https://sas-study.tistory.com/362?category=784778 참고하기
@@ -23,6 +29,16 @@ import static com.yushin.handler.ex.ErrorCode.DUPLICATE_MEMBER;
 @Component
 @Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private ObjectMapper objectMappger = new ObjectMapper();
+    ErrorCode err =ErrorCode.NOT_MATCHED_PASSWORD;
+
+    ErrorResponse errorResponse =  ErrorResponse.builder()
+            .status(err.getHttpStatus().value())
+            .error(err.getHttpStatus().name())
+            .code(err.name())
+            .message(err.getDetail())
+            .build();
+
 
 
     @Override
@@ -30,9 +46,16 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
 
+
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
         log.error("UnAuthorizaed!!! message : " + authException.getMessage());
-        throw new CustomException(ErrorCode.NOT_MATCHED_PASSWORD);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        try (OutputStream os = response.getOutputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(os, errorResponse);
+            os.flush();
+        }
     }
 
 }
--- src/main/java/com/yushin/service/AuthService.java
+++ src/main/java/com/yushin/service/AuthService.java
@@ -47,12 +47,10 @@ public class AuthService {
         }
         // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
         UsernamePasswordAuthenticationToken authenticationToken = loginDto.toAuthentication();
-
         // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
         //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
         //     인증에 성공하면 Authentication을 SecurityContextHolder에 저장할 수 있는 객체로 만들어진다.
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-
         // 3. 인증 정보를 기반으로 JWT 토큰 생성
         TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
 
